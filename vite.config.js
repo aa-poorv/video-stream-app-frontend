542fcf3d1b3e7addc777a4ad1877ddc737d56a11
Apoorv Pandey <apoorvrpandey@gmail.com>
2023-12-04 23:34:41 +0530

    New-deployment-2

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "https://mernvideostreambackend.onrender.com",
         changeOrigin: true,
-        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-  plugins: [react()],
 });
