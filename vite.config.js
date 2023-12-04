1a7441cc2b64be2e82a6aee090893dfcfb3f12bb
Apoorv Pandey <apoorvrpandey@gmail.com>
2023-12-04 23:07:34 +0530

    New Deployment

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://mernvideostreambackend.onrender.com",
-        secure: false,
+        secure: true,
       },
     },
   },
